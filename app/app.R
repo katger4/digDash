7cebdf1a01b2451d8a2fb0e20e849b2a124b34b4
katger4 <kajgertz@gmail.com>
2019-07-11 16:45:03 -0400

    create trans tab, begin overview layout

--- app/app.R
+++ app/app.R
@@ -39,11 +39,12 @@ ui <- dashboardPage(title="Digital dashboard",
                     dashboardHeader(title = logo_blue_gradient,titleWidth = 250),
                     dashboardSidebar(width = 250, sidebarMenu(id = "sidebar_menu",
                                                               menuItem("Overview", tabName = "overview", icon = icon("globe"))
+                                                              ,menuItem("Transactions", tabName = "transactions", icon = icon("arrows-alt-h"))
                                                               )),
                     dashboardBody(# hide errors
                       tags$style(type="text/css",
-                                 # ".shiny-output-error { visibility: hidden; }",
-                                 # ".shiny-output-error:before { visibility: hidden; }",
+                                 ".shiny-output-error { visibility: hidden; }",
+                                 ".shiny-output-error:before { visibility: hidden; }",
                                  "-webkit-font-smoothing: antialiased;",
                                  "-webkit-filter: blur(0.000001px);"
                       ),
@@ -53,28 +54,34 @@ ui <- dashboardPage(title="Digital dashboard",
                                                      ,title = span(HTML("<strong>Daily Digital Data Drop dashboard</strong>")) 
                                                      ,solidHeader = TRUE #,includeHTML("about.html")
                                                      ,status = "primary"
-                                                    ,column(width=4)
-                                                    ,column(width=4,valueBoxOutput("tot", width = NULL))
-                                                    ,column(width=4)
                                                     ))
+                                       ,fluidRow(column(width=4),column(width=4,valueBoxOutput("card_tot", width = NULL)),column(width=4))
+                                       ,fluidRow(column(width = 4, box(width = NULL, chartOutput(outputId = "user_plot_sum", "nvd3"),plotOutput("plot_for_size1")))
+                                                 ,column(width = 4,box(width = NULL, chartOutput(outputId = "views_plot_sum", "nvd3"),plotOutput("plot_for_size2")))
+                                                 ,column(width = 4,box(width = NULL, chartOutput(outputId = "trans_plot_sum", "nvd3"),plotOutput("plot_for_size3"))))
+                                       ,fluidRow(column(width=3),column(width=3,valueBoxOutput("kan_visits", width = NULL)),column(width=3,valueBoxOutput("kan_plays", width = NULL)),column(width=3))
+                                       ),
+                               tabItem(tabName = "transactions"
+                                       ,fluidRow(column(width=4),column(width=4,valueBoxOutput("tot", width = NULL)),column(width=4))
                                        ,valueBoxOutput("t1",width = 3)
                                        ,valueBoxOutput("t2",width = 3)
                                        ,valueBoxOutput("t3",width = 3)
                                        ,valueBoxOutput("t4",width = 3)
                                        ,fluidRow(
                                          column(width = 9
-                                               ,fluidRow(box(width = 12, chartOutput(outputId = "trans_plot", "nvd3"),plotOutput("plot_for_size")))
-                                               )
+                                                ,fluidRow(box(width = 12, chartOutput(outputId = "trans_plot", "nvd3"),plotOutput("plot_for_size")))
+                                                )
                                          ,column(width = 3
-                                               ,fluidRow(box(width=NULL
-                                                              ,radioButtons(inputId = "vars", label = "Variable", choices = var_choices,
-                                                                            selected = "sierra_trans")
-                                                              ,radioButtons(inputId = "time_var", label = "Time", choices = c("monthly","daily"),
-                                                                            selected = "monthly")))
-                                               )
-                                         )
-                                       )
-                               )
+                                                 ,fluidRow(box(width=NULL
+                                                               ,radioButtons(inputId = "vars", label = "Variable", choices = var_choices,
+                                                                             selected = "sierra_trans")
+                                                             ,radioButtons(inputId = "time_var", label = "Time", choices = c("monthly","daily"),
+                                                                           selected = "monthly")))
+                         )
+                       )
+                      ))
+                                       
+                               
                       )
                     )
 
@@ -109,21 +116,46 @@ server <- function(input, output, session) {
   })
   
   ### VALUE BOXES ###
+  
+  output$card_tot <- renderValueBox({
+    text <- HTML(paste("New card sign ups to date",br(),"<span style='font-size:12px'>Jan 2019 - Jun 2019</span>"))
+    valueBox(
+      comma_format()(sum(df %>% select(new_card_sign_ups))), text, icon = icon("id-card"),
+      color = "blue"
+    )
+  })
+  
+  output$kan_visits <- renderValueBox({
+      text <- HTML(paste("Kanopy visits to date",br(),"<span style='font-size:12px'>Jan 2019 - Jun 2019</span>"))
+    valueBox(
+      comma_format()(sum(df %>% select(kanopy_visits))), text, icon = icon("eye"),
+      color = "blue"
+    )
+  })
+  
+  output$kan_plays <- renderValueBox({
+      text <- HTML(paste("Kanopy plays to date",br(),"<span style='font-size:12px'>Jan 2019 - Jun 2019</span>"))
+    valueBox(
+      comma_format()(sum(df %>% select(kanopy_plays))), text, icon = icon("youtube"),
+      color = "blue"
+    )
+  })
+  
   output$tot <- renderValueBox({
     v <- if (input$vars == "sierra_trans") {'Sierra'}
             else if (input$vars == "overdrive_trans") {'Overdrive'}
             else if (input$vars == "cloud_trans") {'CloudLibrary'}
     text <- HTML(paste(v, "transactions to date",br(),"<span style='font-size:12px'>Jan 2019 - Jun 2019</span>"))
     valueBox(
-      comma_format()(sum(var()$count)), text, icon = icon("book"),
+      comma_format()(sum(var()$count)), text, icon = icon("stopwatch"),
       color = "blue"
     )
   })
   
   output$t1 <- renderValueBox({
     v_type <- if (input$vars == "sierra_trans") {'sierra_checkins'}
-    else if (input$vars == "overdrive_trans") {'overdrive_ebook_holds'}
-    else if (input$vars == "cloud_trans") {'cloudlibrary_ebook_holds'}
+    else if (input$vars == "overdrive_trans") {'overdrive_ebook_checkouts'}
+    else if (input$vars == "cloud_trans") {'cloudlibrary_ebook_checkouts'}
     text <- HTML(paste(var_to_label(v_type), "to date",br(),"<span style='font-size:12px'>Jan 2019 - Jun 2019</span>"))
     valueBox(
       comma_format()(sum(var() %>% filter(transaction_type == v_type) %$% count)), text, icon = icon("book"),
@@ -160,7 +192,7 @@ server <- function(input, output, session) {
     text <- HTML(paste(var_to_label(v_type), "to date",br(),"<span style='font-size:12px'>Jan 2019 - Jun 2019</span>"))
     if (!input$vars == "sierra_trans"){
       valueBox(
-      comma_format()(sum(var() %>% filter(transaction_type == v_type) %$% count)), text, icon = icon("bookmark"),
+      comma_format()(sum(var() %>% filter(transaction_type == v_type) %$% count)), text, icon = icon("book-reader"),
       color = "blue"
     )}
   })
